49b1c53d90e7fcf77e4591f2b62483a63627ded6
esteban-madrigal-ak <emadrigal@akurey.com>
2022-08-25 17:11:20 -0600

    Adding scorecard, level and cursor icon

--- Task12-whack-a-mole/src/game.js
+++ Task12-whack-a-mole/src/game.js
@@ -6,14 +6,22 @@ function Game() {
     const scoreBoard = document.querySelector('.score');
     const game = document.querySelector('.game');
     const moles = document.querySelectorAll('.mole');
+    const scoreCard = document.querySelector('.scores');
+    const inputName = document.querySelector('#pname');
+    const levelText = document.querySelector('.level');
+
     const minPeepTime = 200;
-    const maxPeepTime = 1000;
+    let maxPeepTime = 1000;
     const failAudio = "kick";
     const hitAudio = "hihat"
     const elementToHit = "mole";
+
+    let players = [];
     let lastHole;
     let timeUp = false;
     let score = 0;
+    let playerName = "";
+    let level = 1;
 
     this.initialize = () => {
         game.addEventListener('click', this.bonk);
@@ -42,24 +50,67 @@ function Game() {
     }
 
     this.startGame = () => {
+        playerName = (inputName.value == "") ? "Anonymous" : inputName.value;
+        
         scoreBoard.textContent = 0;
-        timeUp = false;
         score = 0;
+        levelText.textContent = 1;
+        level = 1;
+
+        timeUp = false;
         this.peep();
-        setTimeout(() => timeUp = true, 10000)
+        setTimeout(() => this.finishGame(), 10000)
+    }
+
+    this.sortPlayers = () => {
+        players.sort((player1, player2) => {
+            if (player1.score < player2.score) {
+                return 1;
+            }
+            else if (player1.score > player2.score) {
+                return -1;
+            }
+            return 0;
+        });
+    }
+
+    this.finishGame = () => {
+        timeUp = true;
+        players.push({ 'name': playerName, 'score': score });
+        console.table(players);
+        this.sortPlayers();
+        this.updateScores();
+        alert("El juego acabo!");
+        console.table(players);
+    }
+
+    this.updateScores = () => {
+        scoreCard.innerHTML = "";
+        players.slice(0 , 5).forEach(player =>{
+            scoreCard.innerHTML += `<b>${player.name} ${player.score}</b><br>`;
+        })
     }
 
     this.bonk = e => {
         if (!e.isTrusted) return;
         let audioToPlay = failAudio
-        if (e.target.className == elementToHit){
+        if (e.target.className == elementToHit) {
             score++;
             e.target.classList.remove('up');
             scoreBoard.textContent = score;
+            this.manageLevels();
             audioToPlay = hitAudio;
         }
-        let audio = document.getElementById(audioToPlay); 
+        let audio = document.getElementById(audioToPlay);
         audio.play();
+    }
+
+    this.manageLevels = () => {
+        if(score > 5){
+            maxPeepTime = 700;
+            level++;
+            levelText.textContent = level;
+        }
     }
 }
 
